4a6478b295374e17a23dab86ebf5434a062dba21
Tayyab0505 <tayyabshakeel005@gmail.com>
2026-03-15 05:33:43 +0500

    Add verify token in auth Middleware.

--- backend/middleware/authMiddleware.js
+++ backend/middleware/authMiddleware.js
@@ -3,9 +3,9 @@ const jwt = require('jsonwebtoken');
 const verifyToken = async (req, res, next) => {
     const authHeader = req.headers.authorization;
 
-    if (!authHeader || !authHeader.startsWith("Bearer")) {
-        return res.status(401).json({ message: "No token provided" });
-    };
+    if (!authHeader || !authHeader.startsWith('Bearer')) {
+        return res.status(401).json({ message: 'No token provided' });
+    }
     const token = authHeader.split(" ")[1];
 
     try {
@@ -14,8 +14,8 @@ const verifyToken = async (req, res, next) => {
         next();
     } catch (error) {
         return res.status(403).json({ message: "Invalid or expired token", error: error.message });
-    };
-};
+    }
+}
 
 const verifyAdmin = (req, res, next) => {
     const token = req.headers.authorization?.split(" ")[1];
