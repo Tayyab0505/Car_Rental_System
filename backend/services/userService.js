78f36dc2ed4647b81548578f3fafeb871626c328
Tayyab0505 <tayyabshakeel005@gmail.com>
2026-03-16 23:52:26 +0500

    set default value 'user' in userService.js

--- backend/services/userService.js
+++ backend/services/userService.js
@@ -24,7 +24,7 @@ const register = async (data) => {
     };
 
     const hashedPassword = await bcrypt.hash(password, 10);
-    const user = await User.create({ name, email, password: hashedPassword, role });
+    const user = await User.create({ name, email, password: hashedPassword, role: 'user' });
 
     return user;
 };
